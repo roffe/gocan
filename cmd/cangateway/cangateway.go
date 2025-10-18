451245448af45ad1badc1dc14fb5a7384af5fe03
Roffe <joakim@roffe.nu>
2025-10-18 23:20:26 +0200

    add systray & icon to cangateway

--- cmd/cangateway/cangateway.go
+++ cmd/cangateway/cangateway.go
@@ -8,11 +8,18 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/energye/systray"
+
+	_ "embed"
 )
 
 var (
 	ignoreQuit bool
 )
+
+//go:embed Icon.ico
+var iconData []byte
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
@@ -35,10 +42,37 @@ func main() {
 	// Handle SIGINT and SIGTERM for graceful shutdown
 	signalHandler(srv)
 
+	go systray.Run(onReady(srv), onExit)
+
 	// Run the server
 	if err := srv.Run(); err != nil && !errors.Is(err, net.ErrClosed) {
 		log.Fatalf("server: %v", err)
 	}
+}
+func onReady(srv *Server) func() {
+	return func() {
+		systray.SetIcon(iconData)
+		systray.SetTitle("goCAN Gateway")
+		systray.SetTooltip("goCAN Gateway")
+		systray.SetOnClick(func(menu systray.IMenu) {
+			menu.ShowMenu()
+		})
+		systray.SetOnRClick(func(menu systray.IMenu) {
+			menu.ShowMenu()
+		})
+		mQuit := systray.AddMenuItem("Quit", "Quit")
+		mQuit.Click(func() {
+			if err := srv.Close(); err != nil {
+				log.Fatalf("failed to close server: %v", err)
+			}
+		})
+		// Sets the icon of a menu item.
+		//mQuit.SetIcon(icon.Data)
+	}
+}
+
+func onExit() {
+	// clean up here
 }
 
 // signalHandler sets up a signal handler to gracefully shut down the server on SIGINT or SIGTERM.
