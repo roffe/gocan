d282a1e75a86756f55beac3f8bdc274e73087f1a
Roffe <joakim@roffe.nu>
2025-11-20 22:30:43 +0100

    tidy up

--- adapter.go
+++ adapter.go
@@ -40,30 +40,19 @@ func (a *AdapterCapabilities) String() string {
 }
 
 type AdapterConfig struct {
-	Debug                  bool
-	Port                   string
-	PortBaudrate           int
-	CANRate                float64
-	CANFilter              []uint32
-	UseExtendedID          bool // only used for j2534 when setting upp frame filters
-	PrintVersion           bool
-	MinimumFirmwareVersion string
-	AdditionalConfig       map[string]string // for future use
+	Debug            bool              // enable debug logging
+	Port             string            // port name or path to dll/so/dylib
+	PortBaudrate     int               // port baudrate in bps (only used for serial based adapters)
+	CANRate          float64           // CAN bus rate in kbit/s
+	CANFilter        []uint32          // CAN ID filters
+	UseExtendedID    bool              // only used for j2534 when setting upp frame filters
+	PrintVersion     bool              // print adapter version info on open
+	AdditionalConfig map[string]string // Key value pairs for adapter specific configuration
 }
 
 var adapterMap = make(map[string]*AdapterInfo)
 
 func NewAdapter(adapterName string, cfg *AdapterConfig) (Adapter, error) {
-	//if cfg.OnMessage == nil {
-	//	cfg.OnMessage = func(msg string) {
-	//		_, file, no, ok := runtime.Caller(1)
-	//		if ok {
-	//			fmt.Printf("%s#%d %v\n", filepath.Base(file), no, msg)
-	//		} else {
-	//			log.Println(msg)
-	//		}
-	//	}
-	//}
 	if adapter, found := adapterMap[adapterName]; found {
 		return adapter.New(cfg)
 	}
