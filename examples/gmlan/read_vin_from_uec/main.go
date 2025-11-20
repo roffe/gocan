55224dd723cb5d6e32ee8d7880d8db6a9627bcb5
Roffe <joakim@roffe.nu>
2025-11-20 22:31:21 +0100

    update example

--- examples/gmlan/read_vin_from_uec/main.go
+++ examples/gmlan/read_vin_from_uec/main.go
@@ -14,41 +14,50 @@ func main() {
 		&gocan.AdapterConfig{
 			Port:         `C:\Program Files (x86)\Drew Technologies, Inc\J2534\MongoosePro GM II\monpa432.dll`,
 			PortBaudrate: 0,
-			CANRate:      33.3,
+			CANRate:      500,
 			CANFilter:    []uint32{0x64F},
 		},
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	c, err := gocan.NewWithOpts(ctx, dev)
+
+	cl, err := gocan.NewWithOpts(ctx, dev)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer c.Close()
 
-	gm := gmlan.New(c, 0x24F, 0x64F)
+	go func() {
+		defer cl.Close()
+		//gm := gmlan.New(cl, 0x24F, 0x64F)
+		gm := gmlan.New(cl, 0x7E0, 0x7E8)
 
-	gm.TesterPresentNoResponseAllowed()
+		gm.TesterPresentNoResponseAllowed()
 
-	if err := gm.InitiateDiagnosticOperation(ctx, 0x02); err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		if err := gm.ReturnToNormalMode(ctx); err != nil {
-			log.Println(err)
+		if err := gm.InitiateDiagnosticOperation(ctx, 0x02); err != nil {
+			log.Fatal(err)
 		}
+		defer func() {
+			if err := gm.ReturnToNormalMode(ctx); err != nil {
+				log.Println(err)
+			}
+		}()
+
+		if err := gm.DisableNormalCommunication(ctx); err != nil {
+			log.Fatal(err)
+		}
+
+		vin, err := gm.ReadDataByIdentifierString(ctx, 0x90)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("VIN:", vin)
 	}()
 
-	if err := gm.DisableNormalCommunication(ctx); err != nil {
+	if err := cl.Wait(ctx); err != nil {
 		log.Fatal(err)
 	}
-
-	vin, err := gm.ReadDataByIdentifierString(ctx, 0x90)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("VIN:", vin)
 }
